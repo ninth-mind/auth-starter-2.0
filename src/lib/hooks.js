2f43335d33118655a79a737f32fd58f481a7ad0b
Jamie Skinner <skinner520@gmail.com>
2019-09-10 12:43:54 -0500

    refactor: add useProfile hook, currently not using

--- src/lib/hooks.js
+++ src/lib/hooks.js
@@ -1,1 +1,25 @@
-// import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+export function useServerProfile(profile) {
+  let [newProfile, setProfile] = useState(profile)
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        let r = await axios({
+          method: 'get',
+          url: `http://localhost:3000/api/me`
+        })
+        setProfile(r.data.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    fetchData()
+
+    return
+  }, [newProfile])
+
+  return newProfile
+}
